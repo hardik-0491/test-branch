c327a441f0ebead9c3e3947fe6c16ec6805e89a3
Hardik <hardik.rsenghani@gmail.com>
2021-12-31 16:19:34 +0530

    Making changes for Request

--- src/com/hs/Main.java
+++ src/com/hs/Main.java
@@ -6,10 +6,15 @@ public class Main {
         System.out.println("Hello World1 !!!");
         String data = "This line is for branch-hs";
         test();
+        createPullRequest();
     }
 
     public static void test() {
         String val = "This is test method";
+    }
+
+    public static void createPullRequest(){
+        String val1 = "This is for pull request";
     }
 
 }
